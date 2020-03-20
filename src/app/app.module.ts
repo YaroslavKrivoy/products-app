53b671188df65349ac1503412c20596fe8a5a4b2
krivoy.znu@gmail.com <Yaroslav Krivoy>
2020-03-20 19:00:36 +0200

    Create component product-alerts and add styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductAlertsComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule,
