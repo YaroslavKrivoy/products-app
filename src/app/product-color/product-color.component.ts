65432be8c947d0b194233206234754439d42a596
krivoy.znu@gmail.com <Yaroslav Krivoy>
2020-03-24 13:24:31 +0200

    Create component product-color

---
+++ src/app/product-color/product-color.component.ts
@@ -0,0 +1,41 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {ColorModel} from '../core/models/Product';
+
+export enum ValueChangeCount {
+  Plus = 'PLUS',
+  Minus = 'MINUS'
+}
+
+@Component({
+  selector: 'app-product-color',
+  templateUrl: './product-color.component.html',
+  styleUrls: ['./product-color.component.scss']
+})
+export class ProductColorComponent implements OnInit {
+
+  @Input()
+  productColorItem: ColorModel;
+
+  @Output() changeCount = new EventEmitter<ValueChangeCount>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onClickPlus() {
+    this.changeCount.emit(ValueChangeCount.Plus);
+    this.productColorItem.count++;
+  }
+
+  onClickMinus() {
+    if (this.productColorItem.count > 0) {
+      this.changeCount.emit(ValueChangeCount.Minus);
+      this.productColorItem.count--;
+    }
+  }
+
+  getColor() {
+    return this.productColorItem.color;
+  }
+}
