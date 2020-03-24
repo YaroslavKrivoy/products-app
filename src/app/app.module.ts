b22781817694c675b9f5ee1d75ed62dd9ae46e6d
krivoy.znu@gmail.com <Yaroslav Krivoy>
2020-03-24 13:28:09 +0200

    Add Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,27 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductColorComponent } from './product-color/product-color.component';
+import { ProductColorListComponent } from './product-color-list/product-color-list.component';
+import { ProductComponent } from './product/product.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatChipsModule} from '@angular/material/chips';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ProductAlertsComponent,
-    TopBarComponent
+    TopBarComponent,
+    ProductColorComponent,
+    ProductColorListComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule,
+    MatChipsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
